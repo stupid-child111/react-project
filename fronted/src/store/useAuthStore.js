5910b81538309e73599f4bd3f6d9974a2c6009f8
stupid-child111 <3468619937@qq.com>
2025-07-02 01:24:09 +0800

    feat: 用户个人资料页面完成 && 上传图像问题

--- fronted/src/store/useAuthStore.js
+++ fronted/src/store/useAuthStore.js
@@ -41,11 +41,38 @@ export const useAuthStore = create((set) => ({
     logout: async () => {
         try {
             await axiosInstance.post("auth/logout");
-            set({authUser: null});
+            set({ authUser: null });
             toast.success("您已成功登出");
         } catch (error) {
             toast.error("登出失败，请稍后再试");
             console.log("Error in logout", error.response.data.message);
         }
+    },
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("登录成功");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in login", error);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+    updateProfile: async (data) => { 
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("个人资料更新成功");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in updateProfile", error);
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
     }
+
 }));

2a6161669ec758fd597ea2db03ed9111ae003f84
stupid-child111 <3468619937@qq.com>
2025-07-03 02:09:07 +0800

    feat: 页面切换主题实现 && 只有light/dark有变化

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -7,42 +7,43 @@ import LoginPage from './pages/LoginPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { useAuthStore } from './store/useAuthStore.js'
+import { useThemeStore } from './store/useThemeStore.js'
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
-
+  const { theme } = useThemeStore()
   useEffect(() => {
     checkAuth()
   }, [checkAuth]);
   console.log({ authUser })
 
-  if (isCheckingAuth && !authUser)return(
+  if (isCheckingAuth && !authUser) return (
     <div className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
     </div>
-  ) 
+  )
 
 
 
-    return (
-      <>
-        <NavBar />
-        <Routes>
-          {/* <Route path="/" element={authUser ? <HomePage /> : <LoginPage />} /> */}
-          {/* 上面这种有什么不好的 */}
-          <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-          <Route path="/setting" element={<SettingsPage /> } />
-          <Route path="/profile" element={authUser? <ProfilePage /> : <Navigate to="/login" />} />
-          {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
-        </Routes>
+  return (
+    <div data-theme={theme}>
+      <NavBar />
+      <Routes>
+        {/* <Route path="/" element={authUser ? <HomePage /> : <LoginPage />} /> */}
+        {/* 上面这种有什么不好的 */}
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/setting" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
+      </Routes>
 
-        <Toaster />
-      </>
-    )
-  }
+      <Toaster />
+    </div>
+  )
+}
 
-  export default App
+export default App
--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -1,5 +1,5 @@
 // tailwind.config.js
-import daisyui from 'daisyui'
+import daisyui from 'daisyui';
 
 export default {
   content: [
@@ -10,4 +10,40 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+     themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  }
 }
